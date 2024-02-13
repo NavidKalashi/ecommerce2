fe5f760080141c6ad456f6eb868121a34b0eb1d1
NavidKalashi <kalashinavid@gmail.com>
2024-02-13 16:48:45 +0330

    first commit

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def products(request):
+    return render(request, 'products/products.html')
+
+def product(request, pk):
+    return render(request, 'products/product.html', {'pk': pk})
