1369dad9a6b4d7c8da53825fb9103b3148a06618
NavidKalashi <kalashinavid@gmail.com>
2024-02-14 19:06:53 +0330

    signup function

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+        
+        messages.success(request, 'your account has been successfully created.')
+
+        return redirect('login')
+
     return render(request, 'authentication/signup.html')
     
 def login(request):
