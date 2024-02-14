491d88c7f0a2a9df3986c5c004cada64a42d6d75
NavidKalashi <kalashinavid@gmail.com>
2024-02-14 16:28:14 +0330

    create models

--- products/views.py
+++ products/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Products
+
 # Create your views here.
 
 def products(request):
-    return render(request, 'products/products.html')
+    products = Products.objects.all()
+    context = {'products': products}
+    return render(request, 'products/products.html', context)
 
 def product(request, pk):
-    return render(request, 'products/product.html', {'pk': pk})
+    product = Products.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'products/product.html', context)
