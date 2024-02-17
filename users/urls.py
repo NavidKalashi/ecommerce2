d9867446e565c9b747a2407b1ffa42c2c0aff5a4
NavidKalashi <kalashinavid@gmail.com>
2024-02-17 20:00:13 +0330

    add signal

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('logout/', views.logoutUser, name='logout'),
     
-    # path('profile/<str:pk>/', views.profile, name='profile'),
+    path('profile/<str:pk>/', views.userProfile, name='profile'),
     path('account/', views.userAccount, name='account'),
     path('edit-account/', views.editAccount, name='edit-account'),
 ]
--- users/views.py
+++ users/views.py
@@ -9,26 +9,27 @@ from .models import Profile
 
 def loginUser(request):
     page = 'login'
-
-    if request.user.is_authenticated:
-        return redirect('products') 
     
-    if request.method == 'post':
-        username = request.POST['username']
+    if request.user.is_authenticated:
+        return redirect('products')
+    
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
         password = request.POST['password']
+        
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request, 'username does not exist')
-            
-        user = authenticate(request, username=username, password=password)
-        
-        if request is not None:
-            login(request, user)
-            return redirect('products')
-        else:
-            messages.error(request, 'username or password are incorrect')
+            messages.error(request, 'username does not exsit')
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+        else:
+            messages.error(request, 'Username or passwrod is incorrect')
+        
     return render(request, 'users/login-register.html')
 
 def registerUser(request):
@@ -40,11 +41,12 @@ def registerUser(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
+            messages.success(request, 'User account was created!')
             user.save()
             login(request, user)
             return redirect('edit-account')
         else:
-            return redirect('profiles')
+           messages.error(request, 'An error has occurred during registeration')
         
     context = {'page': page, 'form': form}
     return render(request, 'users/login-register.html', context)
@@ -53,16 +55,18 @@ def logoutUser(request):
     logout(request)
     return redirect('login')
 
-# def profile(request, pk):
-#     profile = Profile.objects.get(id=pk)
-#     context = {'profile': profile}
-#     return render(request, 'users/user-profile.html', context)
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+    return render(request, 'users/user-profile.html', context)
 
+@login_required(login_url='login')
 def userAccount(request):
     profile = request.user.profile
     context = {'profile': profile}
     return render(request, 'users/account.html', context)
 
+@login_required(login_url='login')
 def editAccount(request):
     profile = request.user.profile
     form = ProfileForm(instance=profile)
