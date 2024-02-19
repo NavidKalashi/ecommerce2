4c174b131cab9f8b9fd7964327fd5e50dc36592d
NavidKalashi <kalashinavid@gmail.com>
2024-02-19 20:09:17 +0330

    add js file and change some views

--- products/views.py
+++ products/views.py
@@ -16,14 +16,10 @@ def product(request, pk):
 
 def cart(request):
     if request.user.is_authenticated:
-        profile = request.user.profile
-        order, created = Order.objects.get_or_create(customer=profile, complete=False)
+        customer = request.user.profile
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItem = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
-        cartItems = order['get_cart_items']
-        
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    context = {'items': items}
     return render(request, 'products/cart.html', context)
